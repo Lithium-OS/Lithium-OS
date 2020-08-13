baa643a974faaccfa7d1d48db3525239639bfcaa
AlanCui <AlanCui4080@outlook.com>
2020-08-13 10:51:38 -0700

    fix

--- src/inc/realmode/disk.asm
+++ src/inc/realmode/disk.asm
@@ -1,8 +1,8 @@
 ;ax 存扇区数 ebx 存开始扇区编号 ecx 段地址:偏移地址
 LBAReadSector:
-    mov [SYM_DISK_ASM_DAP_CACHE_COUNT],ax
-    mov [SYM_DISK_ASM_DAP_CACHE_STARTS],ebx
-    mov [SYM_DISK_ASM_DAP_CACHE_TARGET],ecx
+    mov [0x7e06],ax
+    mov [0x7e0c],ebx
+    mov [0x7e08],ecx
     mov ah,0x42
     mov dl,0x80
-    mov si,SYM_DISK_ASM_DAP_CACHE
+    mov si,0x7e04
