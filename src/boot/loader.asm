df376c7e8ae5addc4cd5e0f7b9cc0ac2264e3618
AlanCui <AlanCui4080@outlook.com>
2020-08-18 21:30:29 +0800

    up

--- src/boot/loader.asm
+++ src/boot/loader.asm
@@ -6,23 +6,23 @@ jmp StartLoader
 nop
 
 GDT_Begin: dd 0, 0
-                            ;Data R/W 4KiB Kernel Base:0 Limit:512MiB
+                            ;Data R/W 4KiB Kernel Base:0 Limit:512MiB               0x08
 GDT_Kernel_dataSect:    dq 00000000_1100_0010_10010010_00000000_0000000000000000_0000000000000000b
-                            ;Code ExecOnly 4KiB Kernel Base:0 Limit:512MiB
+                            ;Code ExecOnly 4KiB Kernel Base:0 Limit:512MiB          0x10
 GDT_Kernel_codeSect:    dq 00000000_1100_0010_10011000_00000000_0000000000000000_0000000000000000b
-                            ;Stack R/W 4KiB User Base:512MiB limit:640MiB
+                            ;Stack R/W 4KiB User Base:512MiB limit:640MiB           0x18
 GDT_User_stackSect:     dq 00000000_1100_0010_11110110_00000010_0000000000000000_1000000000000000b
-                            ;Stack R/W 4KiB Kernel Base:642MiB limit:640MiB
+                            ;Stack R/W 4KiB Kernel Base:642MiB limit:640MiB         0x20
 GDT_Kernel_stackSect:   dq 00000000_1100_0010_11110110_00000010_1000001000000000_1000000000000000b
-                            ;Data R/W 4KiB User Base:642MiB Limit:unlimited
+                            ;Data R/W 4KiB User Base:642MiB Limit:unlimited         0x28
 GDT_User_dataSect:      dq 00000000_1100_1111_11110010_00000010_1000001000000000_1111111111111111b
-                            ;Code ExecOnly 4KiB User Base:642MiB Limit:unlimited
+                            ;Code ExecOnly 4KiB User Base:642MiB Limit:unlimited    0x30
 GDT_User_codeSect:      dq 00000000_1100_1111_11111000_00000010_1000001000000000_1111111111111111b
-GDT_End: dd 0, 0
 
-GDT_LEN equ GDT_End - GDT_Begin
+GDT_End:
+GDT_LEN equ GDT_End - GDT_Begin - 1
 GDT_PTR dw GDT_LEN
-    dd GDT_Begin
+        dd GDT_Begin
 
 [BITS 16]
 StartLoader:
@@ -38,27 +38,25 @@ StartLoader:
     mov dx, 0300h
     call ShowMessage
 
-    jmp $
     
     ; open A20 address
-    mov ax,0x2401
-    mov ax,0x180
-    int 0x15
-    jc OpenA20Fail
+    enable_A20
+    mov bx, MagOpenA20OK
+    mov cx, 26
+    mov dx, 0400h
+    call ShowMessage
 
-    mov ax,0x4f02
-    int 13
     cli
 
     db 0x66
     lgdt [GDT_PTR]
 
-    mov ax,0000000000001000b
+    mov ax,0x10
     mov cs,ax
-    mov ax,0000000000010000b
+    mov ax,0x08
     mov ds,ax
 
-    ; open protection mode
+     ; open protection mode
     mov eax, cr0
     or eax, 0x01
     mov cr0, eax
@@ -86,7 +84,53 @@ ShowMessage:
     int 0x10
     ret
 
+enable_A20:
+        cli
+ 
+        call    a20wait
+        mov     al,0xAD
+        out     0x64,al
+ 
+        call    a20wait
+        mov     al,0xD0
+        out     0x64,al
+ 
+        call    a20wait2
+        in      al,0x60
+        push    eax
+ 
+        call    a20wait
+        mov     al,0xD1
+        out     0x64,al
+ 
+        call    a20wait
+        pop     eax
+        or      al,2
+        out     0x60,al
+ 
+        call    a20wait
+        mov     al,0xAE
+        out     0x64,al
+ 
+        call    a20wait
+        sti
+        ret
+ 
+a20wait:
+        in      al,0x64
+        test    al,2
+        jnz     a20wait
+        ret
+ 
+ 
+a20wait2:
+        in      al,0x64
+        test    al,1
+        jz      a20wait2
+        ret
+
 MsgStartLoader db "[loader] Staring Loader..."
 MsgOpenA20Fail db "[loader] Open A20 address failed."
+MagOpenA20OK   db "[loader] Open A20 address OK.    "
 
-times 1848 db 0
+times 1786 db 0
